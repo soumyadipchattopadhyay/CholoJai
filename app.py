ab20f2cf3b82bdec0d68ac045a57f44c95d62473
Soumyadip Chattopadhyay <chattopadhyay.soumyadip78@gmail.com>
2026-06-07 14:10:10 +0530

    Update app.py
    uncommented app.run

--- app.py
+++ app.py
@@ -225,9 +225,9 @@ def plan():
         }), 500
 
 
-'''if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(
         debug=True,
         host="0.0.0.0",
         port=5000
-    )'''
+    )
